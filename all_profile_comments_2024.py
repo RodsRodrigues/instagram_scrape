3447042980acf2cc8d2da3349b1fb0076a81bd0c
Rods Rodrigues <rodsrodrigues1103@gmail.com>
2024-07-31 17:58:28 -0300

    clean code on all_profile_comments_2024

--- all_profile_comments_2024.py
+++ all_profile_comments_2024.py
@@ -1,38 +1,52 @@
 import instaloader
 import csv
+from login import login
 
-ig = instaloader.Instaloader()
+def extract_comments(username, profile):
+    ig = instaloader.Instaloader()
 
-# Load login session
-ig.load_session_from_file('username', 'sessao')
+    # Load login session
+    ig.load_session_from_file(username, 'session')
 
-comments = []
+    comments = []
 
-user_profile = 'profile'
-
-try:
-    profile = instaloader.Profile.from_username(ig.context, user_profile)
+    try:
+        profile = instaloader.Profile.from_username(ig.context, profile)
+        
+        for post in profile.get_posts():
+            if post.date_utc.year == 2024:
+                for comment in post.get_comments():
+                    comments.append((comment.created_at_utc, comment.text))
+                    
+    except instaloader.exceptions.BadResponseException:
+        print("Metadata search failed. Check if the shortcode is correct.")
+    except Exception as e:
+        print(f"Error: {e}")
     
-    for post in profile.get_posts():
-        if post.date_utc.year == 2024:
-            for comment in post.get_comments():
-                comments.append((comment.created_at_utc, comment.text))
-                
-except instaloader.exceptions.BadResponseException:
-    print("Metadados search failed. Check if the shortcode is correct.")
-except Exception as e:
-    print(f"Error: {e}")
+    print("Extraction of comments successful.")
+    return comments
 
 
-# Save comments and created_at on CSV
-csv_file = 'comments.csv'
 
-try:
-    with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        writer.writerow(['Date', 'Comments'])
-        for comment in comments:
-            writer.writerow([comment[0], comment[1]])
-    print(f'Comments saved on file {csv_file}')
-except Exception as e:
-    print(f'An error has occurred on save csv file {e}')
+def create_csv(comments, csv_file='comments.csv'):
+    # Save comments and created_at in a CSV file
+    try:
+        with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow(['Date', 'Comments'])
+            for comment in comments:
+                writer.writerow([comment[0], comment[1]])
+        print(f'Comments saved in file {csv_file}')
+    except Exception as e:
+        print(f'An error occurred while saving the CSV file: {e}')
+
+
+if __name__ == '__main__':
+    print("Starting login.")
+    login()
+
+    print("Starting extraction of comments.")
+    comments = extract_comments('username', 'shortcode')
+
+    print("Writing CSV.")
+    create_csv(comments)
